f6a80d1fc48eba133a520cbece52172510b01fd5
Amliqer <151988410+Amliqer@users.noreply.github.com>
2024-04-22 15:41:41 +0300

    Add files via upload

---
+++ main (2).cpp
@@ -0,0 +1,12 @@
+#include <StarW.h>
+#define CPPHTTPLIB_OPENSSL_SUPPORT
+
+
+int main() {
+	StarWikiWars* app = new StarWikiWars();
+	cout << app->getInfoChar("1") << endl << " ";
+	cout << app->getInfoPlanet("1") << endl << " ";
+	cout << " " << app->getInfoMovie("1") << endl;
+	cout << app->Quit() << endl;
+	return 0;
+}
